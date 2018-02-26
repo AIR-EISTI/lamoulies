3ed8b1d43df7e6f48345fbe1e7412830bf93f39a
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2018-02-26 18:14:37 +0100

    add question & answer models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    answer_type = models.ForeignKey(AnswerType, on_delete=models.CASCADE)
+
+
+class AnswerType(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class AnswerChoice(models.Model):
+    value = models.CharField(max_length=200)
+    answer_type = models.ForeignKey(AnswerType, on_delete=models.CASCADE)
+
+
+class UserAnswer(models.Model):
+    ident = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(AnswerChoice, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
