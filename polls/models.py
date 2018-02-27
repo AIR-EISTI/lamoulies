80b9ee82de2681994e3d895e5bdf56be1c86207c
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2018-02-27 16:03:21 +0100

    add str translation for each models, and use django's User model

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class AnswerType(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
@@ -10,19 +14,24 @@ class Question(models.Model):
     description = models.TextField(blank=True, null=True)
     answer_type = models.ForeignKey(AnswerType, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class AnswerChoice(models.Model):
     value = models.CharField(max_length=200)
-    answer_type = models.ForeignKey(
-        AnswerType, on_delete=models.CASCADE, related_name='choices')
+    answer_type = models.ForeignKey(AnswerType,
+                                    on_delete=models.CASCADE,
+                                    related_name='choices')
 
-
-class UserAnswer(models.Model):
-    ident = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.value
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.ForeignKey(AnswerChoice, on_delete=models.CASCADE)
-    user = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username + " : " + self.question.title + " -> " + self.choice.value
